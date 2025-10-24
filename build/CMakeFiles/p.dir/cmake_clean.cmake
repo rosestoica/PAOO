f9295ac0230ae573b48d85fa3186469c501b501c
rosestoica <117316042+rosestoica@users.noreply.github.com>
2025-10-24 12:37:19 +0000

    tema1

---
+++ build/CMakeFiles/p.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p.dir/src/Book.cpp.o"
+  "CMakeFiles/p.dir/src/Book.cpp.o.d"
+  "CMakeFiles/p.dir/src/main.cpp.o"
+  "CMakeFiles/p.dir/src/main.cpp.o.d"
+  "p"
+  "p.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
